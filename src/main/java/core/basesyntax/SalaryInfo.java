9b6bf2920840107a9fd99a8ff14afd3869960ff8
Denys Basarab <denya2007basarab@gmail.com>
2025-12-19 19:31:23 +0200

    implement getSalaryInfo method

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,37 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    public static final int DATE = 0;
+    public static final int NAME = 1;
+    public static final int WORK_HOURS = 2;
+    public static final int PAY_FOR_HOUR = 3;
+    public static final String REGEX = "\\s+";
+    public static final DateTimeFormatter DATE_TIME_FORMATTER
+            = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);
+        LocalDate localDateTo = LocalDate.parse(dateTo, DATE_TIME_FORMATTER);
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        for (String name : names) {
+            int earnByName = 0;
+            for (String someData : data) {
+                String[] arrayWithDate = someData.split(REGEX);
+                LocalDate date = LocalDate.parse(arrayWithDate[DATE], DATE_TIME_FORMATTER);
+                if (arrayWithDate[NAME].equals(name)) {
+                    if (!date.isBefore(localDateFrom) && !date.isAfter(localDateTo)) {
+                        earnByName += Integer.parseInt(arrayWithDate[WORK_HOURS])
+                                * Integer.parseInt(arrayWithDate[PAY_FOR_HOUR]);
+                    }
+                }
+            }
+            stringBuilder.append(System.lineSeparator())
+                    .append(name).append(" - ").append(earnByName);
+        }
+        return stringBuilder.toString();
     }
 }
